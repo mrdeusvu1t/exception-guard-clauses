cbe7c59ba1728db31998b8c348dbe475c4859e4d
Yury Hryshanau <mr.deusvult@gmail.com>
2020-11-14 17:52:32 +0300

    Up

--- ExceptionGuardClauses/ThrowingExceptions.cs
+++ ExceptionGuardClauses/ThrowingExceptions.cs
@@ -12,14 +12,14 @@ namespace ExceptionGuardClauses
                 return result;
             }
 
-            throw new Exception("c is not a hex char.");
+            throw new ArgumentException("c is not a hex char.");
         }
 
         public static char GetLastCharacter(string str)
         {
             if (string.IsNullOrEmpty(str))
             {
-                throw new ApplicationException($"{nameof(str)} is null.");
+                throw new ArgumentNullException($"{nameof(str)} is null.");
             }
 
             return str[^1];
@@ -29,17 +29,17 @@ namespace ExceptionGuardClauses
         {
             if (string.IsNullOrEmpty(code))
             {
-                throw new Exception($"{nameof(code)} is null.");
+                throw new ArgumentNullException($"{nameof(code)} is null.");
             }
 
             if (code.Length != 4)
             {
-                throw new Exception($"{nameof(code)} has {code.Length} chars.");
+                throw new ArgumentException($"{nameof(code)} has {code.Length} chars.");
             }
 
             if (day <= 0 || day >= 100)
             {
-                throw new ApplicationException($"{nameof(day)} should be greater than zero and less than 100");
+                throw new ArgumentOutOfRangeException($"{nameof(day)} should be greater than zero and less than 100");
             }
 
             return code + day.ToString(CultureInfo.InvariantCulture);
@@ -49,22 +49,22 @@ namespace ExceptionGuardClauses
         {
             if (string.IsNullOrEmpty(hello))
             {
-                throw new NullReferenceException($"{nameof(hello)} is null.");
+                throw new ArgumentNullException($"{nameof(hello)} is null.");
             }
 
             if (addressee is null)
             {
-                throw new NullReferenceException($"{nameof(addressee)} is null.");
+                throw new ArgumentNullException($"{nameof(addressee)} is null.");
             }
 
             if (addressee.Length == 0)
             {
-                throw new ApplicationException($"array is empty");
+                throw new ArgumentException($"{addressee} is empty");
             }
 
             if (index < 0 || index >= addressee.Length)
             {
-                throw new IndexOutOfRangeException($"{index} is out of range");
+                throw new ArgumentOutOfRangeException($"{index} is out of range");
             }
 
             return $"{hello}, {addressee[index]}!";
@@ -74,22 +74,22 @@ namespace ExceptionGuardClauses
         {
             if (indexArray is null)
             {
-                throw new NullReferenceException($"{nameof(indexArray)} is null");
+                throw new ArgumentNullException($"{nameof(indexArray)} is null");
             }
 
             if (indexArray.Length == 0)
             {
-                throw new InvalidProgramException($"{nameof(indexArray)} is empty.");
+                throw new ArgumentException($"{nameof(indexArray)} is empty.");
             }
 
             if (valueArray is null)
             {
-                throw new NullReferenceException($"{nameof(valueArray)} is null");
+                throw new ArgumentNullException($"{nameof(valueArray)} is null");
             }
 
             if (valueArray.Length == 0)
             {
-                throw new InvalidProgramException($"{nameof(valueArray)} is empty.");
+                throw new ArgumentException($"{nameof(valueArray)} is empty.");
             }
 
             if (indexArrayPosition < 0 || indexArrayPosition >= indexArray.Length)
@@ -101,7 +101,7 @@ namespace ExceptionGuardClauses
 
             if (position < 0 || position >= valueArray.Length)
             {
-                throw new AccessViolationException("position is out of range.");
+                throw new IndexOutOfRangeException("position is out of range.");
             }
 
             return valueArray[position];
